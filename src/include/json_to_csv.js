2af93aa18f93aff054ab3ab2ce53f2be03e83ba1
github username <github email address>
2018-03-02 14:55:29 -0800

    can flatten obj

--- src/include/json_to_csv.js
+++ src/include/json_to_csv.js
@@ -1,23 +1,43 @@
 
-function parseObj(obj){
-
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
 }
 
-function isObject(obj){
-  return (obj && typeof item === 'object' && !Array.isArray(obj));
-};
-
-function findType(item){
-  if (isNaN(item)) return 'NaN';
-  if (item == null) return 'null';
-  if (Array.isArray(item)) return 'array';
-  return typeof item;
-};
-
-// Deep Merge without jQuery https://stackoverflow.com/questions/38345937/object-assign-vs-extend
-function mergeDeep(target, ...sources){
+function mergeDeep(target, ...sources) {
   if (!sources.length) return target;
-  //take the first element off the queue
   const source = sources.shift();
 
-};
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+  return mergeDeep(target, ...sources);
+}
+
+function flattenObj(obj, loc){
+  //flatten array using depth first recursive approach
+  //inspired by
+  if (loc === undefined){
+    loc = ""
+  }
+  if(isObject(obj) || Array.isArray(obj)){
+    let data = {}
+    for(let key in obj){
+      let more_data = flattenObj(obj[key], loc + key + "/")
+      data = mergeDeep(data, more_data)
+    }
+    return data
+  }
+  else{
+    data = {}
+    endLoc = loc.substring(0, loc.length - 1)
+    data[endLoc] = obj
+    return data
+  }
+}
