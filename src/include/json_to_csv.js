995603144c3d8925e55a59bcce0eef692d365424
github username <github email address>
2018-03-02 17:30:18 -0800

    added header extraction

--- src/include/json_to_csv.js
+++ src/include/json_to_csv.js
@@ -3,6 +3,12 @@ function isObject(item) {
   return (item && typeof item === 'object' && !Array.isArray(item));
 }
 
+function myType(obj){
+  return Object.prototype.toString.call(obj).replace(/^\[object (.+)\]$/, '$1').toLowerCase();
+}
+
+
+//cite stack overflow
 function mergeDeep(target, ...sources) {
   if (!sources.length) return target;
   const source = sources.shift();
@@ -18,6 +24,23 @@ function mergeDeep(target, ...sources) {
     }
   }
   return mergeDeep(target, ...sources);
+}
+
+function toJsonArr(json) {
+  let queue = [], next = json, lookahead;
+
+  while(queue){
+    if(myType(next)==='array' && next.length > 0){
+      lookahead = next[0];
+      if (myType(lookahead)=== 'array' || myType(lookahead) == 'object' ) return next;
+    }
+    else if(myType(next)==='object'){
+      for(let key in next){
+        queue.push(next[key]);
+      }
+    }
+    next = queue.shift();
+  }
 }
 
 function flattenObj(obj, loc){
@@ -40,4 +63,50 @@ function flattenObj(obj, loc){
     data[endLoc] = obj
     return data
   }
+}
+
+function extractHeaders(flatObj){
+  let header;
+  let headers = {}
+  for(let key in flatObj){
+    header = flatObj[key];
+    header.replace(/.\d\//, "");
+    headers[header] = true;
+  }
+  return headers;
+}
+
+function toCSV(flatObj){
+  headers = extractHeaders(flatObj);
+
+}
+
+function extractRows(flatObj){
+  let rows = [], index, rowHash
+  for(let key in flatObj){
+    index = findIndex(key)
+    header = findHeader(key)
+    if (rows[index]){
+      rowHash = rows[index]
+      rowHash[header] = flatObj[key]
+    }
+    else {
+      rows[index] = {}
+      rowHash = rows[index]
+      rowHash[header] = flatObj[key]
+    }
+  }
+  return rows
+}
+
+function extractHeaders(rows){
+  let count = 0, longest, currentRow
+  for(let rowKey in rows){
+    currentRow = rows[rowKey]
+    if(Object.keys(currentRow).length > count){
+      count = Object.keys(currentRow).length
+      longest = currentRow
+    }
+  }
+  return Object.keys(longest)
 }
