07e11135f51de353a04d1b733d8746c4b1691a92
Alexandra <github email address>
2018-06-04 19:39:40 -0700

    further modal styling

--- src/components/modal/close_button.js
+++ src/components/modal/close_button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CloseButton = (props) => {
   return <div style={{ position: "relative", marginRight: "6px" }}>
-      <button className="close" onClick={props.handleClose}>
+      <button className="customClose" onClick={props.handleClose}>
         <span>&#10006;</span>
       </button>
     </div>;
--- src/components/modal/modal_component.js
+++ src/components/modal/modal_component.js
@@ -5,7 +5,9 @@ const ModalComponent = (props) => {
   const showing = props.isShowing ? "show" : null
   return <div className={"customModal " + showing}>
     <div className="customContent">
+      <div className="customHeader"><h4>Oops!</h4></div>
       <CloseButton/>
+      <hr style={{width: "90%"}} />
       <div className="customModalBody">
         {props.children}
       </div>
